cced431afc312e6b1bc43d9019b7340246a607e9
HanGyo Jeong <hangyojeong@HanGyoui-MacBook-Pro.local>
2018-07-22 16:19:16 +0900

    feat: make Player Swift file

--- AVPlayerPractice/ViewController.swift
+++ AVPlayerPractice/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerContainerView: UIView!
+    
+    private var videoController : VideoPlayer?{
+        for viewController in self.childViewControllers{
+            if let videoController = viewController as? VideoPlayer{
+                return videoController
+            }
+        }
+        return nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //Any
+    }
 }
 
